b725a2e20e2ee06c367c3d2ba09cf5633e92b008
Rishabh Dora <rd.rishabh@outlook.com>
2021-04-20 20:21:35 +0530

    Initial commit

---
+++ src/AddTodo.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function AddTodo({addTodo}) {
+
+    const [title, setTitle] = useState();
+    const [desc, setDesc] = useState();
+    
+    const submit =(e)=> {
+        //prevents from reloading the page.
+        e.preventDefault();
+        if(!title || !desc){
+            alert("Title or description cannot be blank.");
+        } else{
+          addTodo(title, desc);
+          setTitle("");
+          setDesc("");
+        }
+
+        
+    }
+
+    return (
+        <div className="container">
+        <h3 className="text-center">Add a task</h3>
+            <form onSubmit={submit}>
+  <div className="mb-3">
+    <label htmlFor="text" className="form-label">Task Title</label>
+    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="desc" className="form-label">Todo Description</label>
+    <input type="text" value={desc} onChange={(e) => setDesc(e.target.value)} className="form-control" id="desc"/>
+  </div>
+  
+  <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
+</form>
+        </div>
+    )
+}
+
+export default AddTodo
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+
+    
+
+    return (
+        <footer className="bg-dark text-light " >
+            <p className="text-center">
+            Copyright &copy; MyTodolist.com
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
